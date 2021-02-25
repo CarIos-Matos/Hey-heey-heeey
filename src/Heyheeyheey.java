fbf0518bbb0e344827a66cda500aea96d3fe0dc7
chrism01 <chrism01@students.zhaw.ch>
2021-02-25 22:50:55 +0100

    "test mit intelij"

---
+++ src/Heyheeyheey.java
@@ -0,0 +1,17 @@
+public class Heyheeyheey
+{
+  private int test = 1;
+
+
+    public static void main(String[] args)
+    {
+        Heyheeyheey test = new Heyheeyheey();
+        test.testmethod();
+    }
+  private void testmethod()
+  {
+      System.out.println("test");
+      System.out.println(test);
+
+  }
+}
